aa21cfcbd2e5f98df1a78503b31cb8df7ad9f096
onimuraaika <aika.onimura@gmail.com>
2020-10-13 14:17:53 +0900

    [Start/Add]JavaScriptを学ぼう2章/繰り返し文whileの練習

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var banana = 100;
-var apple = 200;
+var max = 10;
+var count = 0;
+var number = 1;
 
-if(banana < apple){
-  alert("バナナはりんごより安いです。");
-} else if(banana == apple){
-  alert("バナナとりんごは同じ値段です。");
-} else if(banana > apple){
-  alert("バナナはりんごより高いです。");
+while(number < max){    //（）の中身がtrueの間は{}内の処理が繰り返される
+  number = number + 2;
+  count = count + 1;
 }
+
+alert('2を足し続けて' + max + 'を超えるのに必要だった回数は' + count + '回です。');
