95f51c8303fd7b582ca8207f66db7c2626467b82
onimuraaika <aika.onimura@gmail.com>
2020-10-13 17:44:08 +0900

    [Start/Add]JavaScriptを学ぼう3章/入力ダイアログを表示する練習

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-function addString(stringA){
-  var greeting = "こんにちは" + stringA;
-  return greeting;
-}
+//prompt()は、入力ダイアログ
+var promptStr = prompt('何か好きな文字を入力してください。');
 
-//alertStringという変数に関数addStringを格納
-//ここで引数に渡したい引数を入れる
-var alertString;
-alertString = addString("しろくまさん。");
-
-// 作成した変数をアラートに表示させる
-alert(alertString);
+alert(promptStr);
